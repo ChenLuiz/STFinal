c7364450fdd729073e16dc2c182ffb197bc220b3
ChenLuiz <80011679+ChenLuiz@users.noreply.github.com>
2023-04-21 15:30:01 -0400

    Updated Keys
    Keys are given to objects based on attributes, same attributes on multiple objects will generate the same key. So I manually assigned key names.

--- datavizprojectcode.py
+++ datavizprojectcode.py
@@ -4,14 +4,15 @@ import numpy as np
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
     "Select Year",
-    ("2016", "2017", "2018", "2019", "2020")
+    ("2016", "2017", "2018", "2019", "2020"),
+    key = "Sidebar"
 )
 
 # Using "with" notation
 with st.sidebar:
     add_radio = st.radio(
         "Select Map",
-        ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train")
+        ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train"
     )
 
 # Columns with individual controls
@@ -21,19 +22,22 @@ col1, col2, col3 = st.columns(3)
 with col1:
    add_selectbox2 = st.selectbox(
     "Select Year",
-    ("2016", "2017", "2018", "2019", "2020")
+    ("2016", "2017", "2018", "2019", "2020"),
+    key = "col1"
    )
 
 with col2:
    add_selectbox3 = st.selectbox(
     "Select Map",
-    ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train")
+    ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train"),
+    key = "col2"
    )
 
 with col3:
    add_selectbox4 = st.selectbox(
     "Select Event",
-    ("Kato", "IEM", "Blast", "ESL")
+    ("Kato", "IEM", "Blast", "ESL"),
+    key = "col3"
    )
 
 # Container with graph
@@ -53,17 +57,20 @@ col4, cols5 = st.columns(2)
 with col4:
    add_selectbox5 = st.selectbox(
     "Select Year",
-    ("2016", "2017", "2018", "2019", "2020")
+    ("2016", "2017", "2018", "2019", "2020"),
+    key = "col4"
    )
 
    add_selectbox6 = st.selectbox(
     "Select Map",
-    ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train")
+    ("Mirage", "Inferno", "Nuke", "Overpass", "Cobblestone", "Dust 2", "Vertigo", "Cache", "Train"),
+    key = "col4-1"
    )
     
    add_selectbox7 = st.selectbox(
     "Select Event",
-    ("Kato", "IEM", "Blast", "ESL")
+    ("Kato", "IEM", "Blast", "ESL"),
+    key = "col4-2"
    )
 
 with col5:
