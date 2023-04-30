1a74589c378cbcb054aeaa0ee16aafd935eeab1e
ChenLuiz <80011679+ChenLuiz@users.noreply.github.com>
2023-04-30 12:50:34 -0400

    Indent fix

--- datavizprojectcode.py
+++ datavizprojectcode.py
@@ -51,26 +51,26 @@ with st.container():
    # You can call any Streamlit command, including custom components:
    label_dict = {1: 'Cache', 2: 'Cobblestone', 3: 'Dust2', 4: 'Inferno', 5: 'Mirage', 6: 'Nuke', 7: 'Overpass', 8: 'Train', 9: 'Vertigo'}
 
-    data_chart1 = pd.read_csv('updated_file_new.csv')
-    data_chart1_labels = pd.DataFrame({'Map': label_dict.values()})
+   data_chart1 = pd.read_csv('updated_file_new.csv')
+   data_chart1_labels = pd.DataFrame({'Map': label_dict.values()})
 
-    # create a chart with the area mark
-    Cumulative_maps = alt.Chart(data_chart1).transform_window(
+   # create a chart with the area mark
+   Cumulative_maps = alt.Chart(data_chart1).transform_window(
         cumulative_count = "count()",
         sort=[{"field": "left_over"}],
         #tooltip = ['left_over']
-    ).mark_area(color="darkseagreen").encode(
+   ).mark_area(color="darkseagreen").encode(
         x = alt.X("left_over:Q", 'Map:N', title = "Map"),
         y = alt.Y("cumulative_count:Q", title = "Times it has been picked"),
-    )
+   )
 
-    # create a chart with the vertical lines
-    Map_ticks = alt.Chart(data_chart1_labels).mark_rule(color='seagreen', strokeWidth=5).encode(
+   # create a chart with the vertical lines
+   Map_ticks = alt.Chart(data_chart1_labels).mark_rule(color='seagreen', strokeWidth=5).encode(
         x=alt.X('Map:N', axis=None),
-    )
+   )
 
-    # layer the two charts and show the result
-    st.altair_chart(Cumulative_maps + Map_ticks, use_container_width=True)
+   # layer the two charts and show the result
+   st.altair_chart(Cumulative_maps + Map_ticks, use_container_width=True)
 
 st.write("This is outside the container")
 
