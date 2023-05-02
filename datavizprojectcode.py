04005df11945f81539af1a5e3649d29d87fe7580
ChenLuiz <80011679+ChenLuiz@users.noreply.github.com>
2023-05-02 03:07:52 -0400

    Title

--- datavizprojectcode.py
+++ datavizprojectcode.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
+st.title("CS:GO Pro match data analysis")
 # Disables stupid max rows for data input
 alt.data_transformers.disable_max_rows()
 
